7ed8f3a66d8726f176f8a2dc69f8b43b313bad90
Miika Alikirri <miika.alikirri@suse.com>
2025-11-24 15:08:20 +0200

    events: don't lose inotify if /etc/default/grub is moved
    this often happens when editing with vi that has swp files enabled as
    vi moves the original file, thus making the oringal inotify invalid

--- src/events/mod.rs
+++ src/events/mod.rs
@@ -7,7 +7,7 @@ pub async fn listen_files(connection: &Connection) -> zbus::Result<()> {
     let mut inotify = Inotify::init().expect("Failed to initialize inotify");
     inotify
         .watches()
-        .add("/etc/default/grub", WatchMask::MODIFY)
+        .add("/etc/default/", WatchMask::MODIFY)
         .expect("Failed to watch /etc/default/grub");
 
     loop {
@@ -16,14 +16,19 @@ pub async fn listen_files(connection: &Connection) -> zbus::Result<()> {
             .read_events_blocking(&mut buffer)
             .expect("Failed to read inotify events");
 
+        // prevent duplicate modify event triggers
+        let mut signaled = false;
         for event in events {
-            if event.mask.contains(EventMask::MODIFY) {
-                connection
-                    .object_server()
-                    .interface("/org/opensuse/bootloader")
-                    .await?
-                    .file_changed()
-                    .await?;
+            if event.mask.contains(EventMask::MODIFY) && !signaled {
+                if event.name.is_some_and(|name| name == "grub") {
+                    signaled = true;
+                    connection
+                        .object_server()
+                        .interface("/org/opensuse/bootloader")
+                        .await?
+                        .file_changed()
+                        .await?;
+                }
             }
         }
     }
