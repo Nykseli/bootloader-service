2ea9df83c8249522a671544a022bf05475c8b559
Miika Alikirri <miika.alikirri@suse.com>
2025-12-09 11:33:28 +0200

    scripts: initialise db in run_dev.sh

--- scripts/run_dev.sh
+++ scripts/run_dev.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 set -e
+cd $(dirname "$0")
+cd ..
 
 if [[ ! -d "tmp" ]]; then
     mkdir tmp
     cp /etc/default/grub tmp
+
+    touch tmp/bootloader.db
+    sqlite3 tmp/bootloader.db < db/grub2.sql
 fi
 
 cargo run --features dev -- --session
